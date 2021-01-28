9a883cac1fcfae578dce015b742780a680ef0989
Geir Sørensen <geir.sorensen@dnvgl.com>
2021-01-28 22:47:07 +0100

    Empty project added

---
+++ src/OctoDumpCmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OctoDumpCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
